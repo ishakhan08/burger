c944f35cfe3bb43af6943711709b851bb2c39c56
Isha Khan <khan.isha08@gmail.com>
2018-05-30 00:03:15 -0500

    working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Delight');
+INSERT INTO burgers (burger_name) VALUES ('Big Mc');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
